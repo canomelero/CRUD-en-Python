2b345785acfc366e25e2a32175269467dc2cceb8
jorge <jcanomelero@gmail.es>
2024-10-22 12:38:50 +0200

    rollbacks

--- pedidos.py
+++ pedidos.py
@@ -1,19 +1,12 @@
+# Librería que permite leer y escribir sobre ficheros
+import os
+
 # Adaptador para PostgreSQL en Python
-import psycopg2 as pg   
+import psycopg2 as pg  
 
-# Conexión a la base de datos (ODBC)
-connection = pg.connect(
-    database = 'pedidos',
-    user = 'ddsiuser',
-    password = 'ddsi',
-    host = 'localhost'
-)
 
-# Se desactiva el commit automático para que no lo realice cada
-# vez que se realiza un INSERT, UPDATE, etc
-connection.autocommit = False
 
-# Variables
+# Variables globales para mostrar el menú por pantalla
 menu = """
     \n.: Menú :.
         1. Borrado y creación de tablas con inserción de 10 tuplas 
@@ -29,198 +22,333 @@ menu2 = """
         3. Cancelar el pedido
         4. Finalizar pedido 
     """
-opcion = int(input(menu))
-opcion2 = ''
 
-# Variables para recoger los datos del pedido y stock
-cpedido = ''
-ccliente = ''
-fecha_pedido = ''
-cproducto = ''
-cantStock = ''
+def ejecucion(connection, cursor) -> None:
+    """Ejecuta las distintas funcionalidades del código.
 
-# Creación de un cursor 
-# Éste permite la ejecución de comandos de PostgreSQL en una base de datos
-cursor = connection.cursor();
-cursor.execute("BEGIN;")
-cursor.execute("SAVEPOINT s1;")
+    Parameters
+    ----------
+    connection : psycopg2.connection
+        Objeto con la conexión realizada a la base de datos.
+    cursor : psycopg2.cursor
+        Cursor a la base de datos de la conexión realizada.
+    
+    Return
+    ------
+    None
+    """
 
-# Contenido
-while opcion != 4:
-    if(opcion == 1):
-        print("Creación de tablas e inserción de tuplas en Stock...")
-        
-        try:
-            crearPedidosScript = """
-                DROP TABLE IF EXISTS stock CASCADE;
-                DROP TABLE IF EXISTS pedido CASCADE;
-                DROP TABLE IF EXISTS detalle_pedido CASCADE;
+    opcion = int(input(menu))
+    opcion2 = str()
 
-                CREATE TABLE IF NOT EXISTS stock (
-                    cproducto VARCHAR(5) PRIMARY KEY,
-                    cantidad INT
-                );
-
-                CREATE TABLE IF NOT EXISTS pedido (
-                    cpedido VARCHAR(5) PRIMARY KEY,
-                    ccliente INT,
-                    fecha_pedido VARCHAR(10) 
-                );
-
-                CREATE TABLE IF NOT EXISTS detalle_pedido (
-                    cpedido VARCHAR(5),
-                    cproducto VARCHAR(5),
-                    cantidad INT,
-                    FOREIGN KEY(cpedido) REFERENCES pedido(cpedido),
-                    FOREIGN KEY(cproducto) REFERENCES stock(cproducto),
-                    PRIMARY KEY(cpedido, cproducto)
-                );
-
-                INSERT INTO stock VALUES ('1a', 4);
-                INSERT INTO stock VALUES ('2a', 2);
-                INSERT INTO stock VALUES ('3a', 5);
-                INSERT INTO stock VALUES ('4a', 1);
-                INSERT INTO stock VALUES ('5a', 2);
-                INSERT INTO stock VALUES ('6a', 8);
-                INSERT INTO stock VALUES ('7a', 6);
-                INSERT INTO stock VALUES ('8a', 9);
-                INSERT INTO stock VALUES ('9a', 3);
-                INSERT INTO stock VALUES ('10a', 4);
-            """
-
-            cursor.execute(crearPedidosScript)
-            connection.commit()
-        except Exception as e:
-            cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-            cursor.execute("RELEASE SAVEPOINT s1;")
-            raise e
-        
-    elif(opcion == 2):
-        # Capturar datos del pedido
-        cpedido = input("Código del pedido: ")
-        ccliente = int(input("Código del cliente: "))
-        fecha_pedido = input("Fecha del pedido: ")  # De tipo DATE o VARCHAR
-
-        # Se introducen los datos en la tabla pedido
-        try:
-            cursor.execute(
-                "INSERT INTO pedido(cpedido, ccliente, fecha_pedido) VALUES (%s, %s, %s);",
-                (cpedido, ccliente, fecha_pedido)
-            )
-        except Exception as e:
-            cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-            cursor.execute("RELEASE SAVEPOINT s1;")
-            raise e
-        
-        # Opciones a realizar con el pedido
-        opcion2 = int(input(menu2))
-
-        while opcion2 == 1 or opcion2 == 2:
-            if(opcion2 == 1):
-                print("Detalles del producto...")
-                cproducto = input("Código del producto: ")
-                cantPedida = int(input("Cantidad de artículos: "))
-
-                try:
-                    cursor.execute("SELECT cantidad FROM stock WHERE cproducto = %s;", (cproducto,))
-
-                    # fectone() -> devuelve una tupla (si la hay) en la BD
-                    cantStock = cursor.fetchone()[0]
-                    
-                    if cantStock < cantPedida:
-                        raise Exception("No hay suficiente cantidad de artículos")
-                except Exception as e:
-                    cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-                    cursor.execute("RELEASE SAVEPOINT s1;")
-                    raise e
-                
-                try:
-                    cursor.execute(
-                        "INSERT INTO detalle_pedido(cpedido, cproducto, cantidad) VALUES (%s, %s, %s);",
-                        (cpedido, cproducto, cantPedida)
-                    )
-                    cursor.execute(
-                        "UPDATE stock SET cantidad = cantidad - %s WHERE cproducto = %s;",
-                        (cantPedida, cproducto)
-                    )
-                except Exception as e:
-                    cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-                    cursor.execute("RELEASE SAVEPOINT s1;")
-                    raise e
-                
-            elif(opcion2 == 2):
-                print("Eliminando detalles del producto...")
-
-                try:
-                    cursor.execute(
-                        "DELETE FROM detalle_pedido WHERE cpedido = %s;",
-                        (cpedido)
-                    )
-                except Exception as e:
-                    cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-                    cursor.execute("RELEASE SAVEPOINT s1;")
-                    raise e 
+    # Variables para recoger los datos del pedido y stock
+    cpedido = str()
+    ccliente = str()
+    fecha_pedido = str()
+    cproducto = str()
+    cantidad_stock = str()
+    savepoint_1 = "SAVEPOINT S1;"
+    
+    # Contenido
+    while opcion != 4:
+        if opcion == 1:
+           cursor.execute(savepoint_1)
+           crear_tablas(cursor)
             
+        elif opcion == 2:
+            cursor.execute(savepoint_1) # CREAR UN NUEVO SAVEPOINT ?????
+
+            # Asignar cada valor de la tupla devuelva a cada variable
+            cpedido, ccliente, fecha_pedido = aniadir_pedido(cpedido, ccliente, 
+                                                            fecha_pedido)
+            # Opciones a realizar con el pedido
             opcion2 = int(input(menu2))
 
-        if(opcion2 == 3):
-            print("Eliminando pedido y sus detalles...")
+            while opcion2 == 1 or opcion2 == 2:
+                # PONER AQUÍ UN SAVEPOINT ?????
+                if opcion2 == 1:
+                    cproducto, cantidad_pedida, cantidad_stock = obtener_cantidad_producto(
+                                                            cproducto, cantidad_stock)
+                    aniadir_detalles_pedido(cpedido, cproducto, cantidad_pedida) 
+                
+                elif opcion2 == 2:
+                    eliminar_detalles_pedido(cpedido)   # NECESARIO HACER UN COMMIT ?????
             
-            try:
-                cursor.execute("DELETE FROM detalle_pedido WHERE cpedido = %s;", (cpedido))
-                cursor.execute("DELETE FROM pedido WHERE cpedido = %s;", (cpedido))    
-            except Exception as e:
-                cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-                cursor.execute("RELEASE SAVEPOINT s1;")
-                raise e
-             
-        elif(opcion2 == 4):
-            print("Guardando los cambios realizados...")
-            connection.commit();
+                opcion2 = int(input(menu2))
+
+            if opcion2 == 3:
+                eliminar_detalles_y_pedido(cpedido)    # NECESARIO HACER UN COMMIT ???? 
+
+            elif opcion2 == 4:
+                print("Guardando los cambios realizados y saliendo...")
+                connection.commit();   
         
-    elif(opcion == 3):
-        print("Listando la Base de Datos...")
-        print("Tabla Pedido:")
+        elif opcion == 3:
+            mostrar_tablas()
+            
+        opcion = int(input(menu));
 
-        try:
-            cursor.execute("SELECT * FROM pedido;")
-            tablaPedido = cursor.fetchall()
 
-            for pedido in tablaPedido:
-                print(pedido)
-        except Exception as e:
-            cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-            cursor.execute("RELEASE SAVEPOINT s1;")
-            raise e 
+def crear_tablas(cursor) -> None:
+    """Crea las tablas en una base de datos a partir de un archivo.
+
+    Parameters
+    ----------
+    cursor : psycopg2.cursor
+        Cursor a la base de datos de la conexión realizada.
+
+    Returns
+    -------
+    None
+    """
+
+    print("Creación de tablas e inserción de tuplas en Stock...")
+
+    # getcwd() -> Devuelve un string que representa el directorio de trabajo actual
+    print("Indique el nombre archivo .sql desde la ruta actual (ponga /nombre_archivo.sql):")
+    path = os.getcwd() + input()
+
+    try:  
+        # Abrir el archivo proporcionado en formato sql, el cual contiene las tablas a crear
+        # junto a algunos datos.
+        with open(path, "r") as archivo:
+            cursor.execute(archivo.read())
+    except Exception as e:
+        print("Error en la lectura del archivo")
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e
+
+    connection.commit()
+
+
+def aniadir_pedido(cpedido: str, ccliente: str, fecha_pedido: str) -> tuple:
+    """Añade una nueva tupla a la tabla pedido
+
+    Parameters
+    ----------
+    cpedido : str
+        Código del pedido a insertar. Es la calve primaria de la tabla pedido.
+    ccliente : str
+        Código del cliente que realiza el pedido.
+    fecha_pedido : str
+        Fecha en la que se ha realizado el pedido.
+
+    Returns
+    -------
+    tuple
+        Valores que ha proporcionado el cliente para añadir el pedido.
+    """
+
+    cpedido = input("Código del pedido: ")
+    ccliente = int(input("Código del cliente: "))
+    fecha_pedido = input("Fecha del pedido: ")  # De tipo DATE o VARCHAR
+
+    try:
+        # Introducir los datos en la tabla pedido.
+        cursor.execute(
+            "INSERT INTO pedido(cpedido, ccliente, fecha_pedido) VALUES (%s, %s, %s);",
+            (cpedido, ccliente, fecha_pedido)
+        )
+    except Exception as e:
+        print("No se ha podido insertar el pedido")
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e
+
+    return (cpedido, ccliente, fecha_pedido)
+    
+
+def obtener_cantidad_producto(cproducto: str, cantidad_stock: int) -> tuple:
+    """Obtiene la cantidad de producto que desea el cliente.
+
+    Esta función obtiene la cantidad del producto porporcionado por su código de producto,
+    que desea el cliente. Además comprueba que no ha solicitado más productos del que 
+    hay disponible en la tabla stock.
+
+    Parameters
+    ----------
+    cproducto : str
+        Código del producto a obtener.
+    cantidad_stock : int
+        Existencias restantes del producto solicitado (sin reducir por la cantida 
+        solicitada de dicho producto).
+
+    Returns
+    -------
+    tuple
+        Código del producto solicitado, la cantidad de dicho producto obtenida y la 
+        cantidad de stock que hay de dicho producto.
+
+    Raises
+    ------
+    Exception
+        En caso de que haya 0 existencias del producto solicitado.
+    Exception
+        Si se solicita más productos de los que hay en la tabla stock.
+    """
+
+    print("Detalles del producto...")
+    cproducto = input("Código del producto: ")
+    cantidad_pedida = int(input("Cantidad de artículos: "))
+
+    try:
+        cursor.execute("SELECT cantidad FROM stock WHERE cproducto = %s;", (cproducto,))
+        cantidad_stock = cursor.fetchone()[0]
+
+        if not cantidad_stock:
+            raise Exception("No hay artículos disponibles")
+        elif int(cantidad_stock) < cantidad_pedida:
+            raise Exception("No hay suficiente cantidad de artículos")
+    except Exception as e:
+        print("No se ha podido obtener la cantidad del artículo") 
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e  
         
-        print("\nTabla Stock:")
+    return (cproducto, cantidad_pedida, cantidad_stock)
 
-        try:
-            cursor.execute("SELECT * FROM stock;")
-            tablaStock = cursor.fetchall()
 
-            for artSotck in tablaStock:
-                print(artSotck)
-        except Exception as e:
-            cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-            cursor.execute("RELEASE SAVEPOINT s1;")
-            raise e 
+def aniadir_detalles_pedido(cpedido: str, cproducto: str, cantidad_pedida: int) -> None:
+    """Añade los valores obtenidos por la función obtener_cantidad_producto() y actualiza
+       las existencias restantes de dicho producto.
 
-        print("\nTabla Detalle-Pedido:")
+    Parameters
+    ----------
+    cpedido : str
+        Código del pedido realizado.
+    cproducto : str
+        Código del producto que se ha solicitado.
+    cantidad_pedida : int
+        Cantidad solicitada del código de producto en específico.
 
-        try:
-            cursor.execute("SELECT * FROM stock;")
-            tablaDetallePed = cursor.fetchall()
+    Returns
+    -------
+    None
+    """
 
-            for detallePedido in tablaDetallePed:
-                print(detallePedido)
-        except Exception as e:
-            cursor.execute("ROLLBACK TO SAVEPOINT s1;")
-            cursor.execute("RELEASE SAVEPOINT s1;")
-            raise e 
+    print("Añadiendo los detalles del pedido...")
 
-    opcion = int(input(menu));
+    try:
+        cursor.execute(
+            "INSERT INTO detalle_pedido(cpedido, cproducto, cantidad) VALUES (%s, %s, %s);",
+            (cpedido, cproducto, cantidad_pedida)
+        )
+        cursor.execute(
+            "UPDATE stock SET cantidad = cantidad - %s WHERE cproducto = %s;",
+            (cantidad_pedida, cproducto)
+        )
+    except Exception as e:
+        print("No ha sido posible añadir los detalles del producto")
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e
 
-cursor.close()
-connection.close()
-print("Programa finalizado")
+
+def eliminar_detalles_pedido(cpedido: str) -> None:
+    """Elimina una tupla de la tabla detalle_pedido.
+
+    Parameters
+    ----------
+    cpedido : str
+        Código del pedido de la tupla a eliminar.
+    
+    Returns
+    -------
+    None
+    """
+
+    print("Eliminando detalles del pedido...")
+
+    try:
+        cursor.execute("DELETE FROM detalle_pedido WHERE cpedido = %s;", (cpedido,))
+    except Exception as e:
+        print("No ha sido posible eliminar los detalles del pedido")
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e
+
+
+def eliminar_detalles_y_pedido(cpedido: str) -> None:
+    """Elimina la tupla de la tabla detalles_pedido y de la tabla pedidos.
+
+    Para eliminar la tupla de la tabla detalles_pedido se llama a la función eliminar_
+    detalles_pedido(). Después de llamar a dicha función, elimina el pedido de la tabla
+    pedido.
+
+    Parameters
+    ----------
+    cpedido : str
+        Código del pedido de la tupla a eliminar.
+
+    Returns
+    -------
+    None
+    """
+
+    print("Eliminando pedido...")
+    
+    try:
+        eliminar_detalles_pedido(cpedido)
+        cursor.execute("DELETE FROM pedido WHERE cpedido = %s;", (cpedido,))
+    except Exception as e:
+        print("No ha sido posible eliminar el pedido")
+        cursor.execute("ROLLBACK TO SAVEPOINT s1;")
+        raise e
+    
+
+def mostrar_tablas() -> None:
+    """Muestra el contenido actual de las tablas que se encuentran en la base de datos
+       en la que estás conectado.
+
+    Returns
+    -------
+    None
+    """
+
+    print("Listando la Base de Datos...")
+    print("Tabla Pedido:")
+
+    cursor.execute("SELECT * FROM pedido;")
+    tablaPedido = cursor.fetchall()
+
+    for pedido in tablaPedido:
+        print(pedido)
+
+    print("\nTabla Stock:")
+
+    cursor.execute("SELECT * FROM stock;")
+    tablaStock = cursor.fetchall()
+
+    for artSotck in tablaStock:
+        print(artSotck)
+
+    print("\nTabla Detalle-Pedido:")
+
+    cursor.execute("SELECT * FROM detalle_pedido;")
+    tablaDetallePed = cursor.fetchall()
+
+    for detallePedido in tablaDetallePed:
+        print(detallePedido)
+
+
+if __name__=='__main__':
+    # Conexión a la base de datos
+    connection = pg.connect(
+        database = 'pedidos',
+        user = 'ddsiuser',
+        password = 'ddsi',
+        host = 'localhost'
+    )
+
+    # Se desactiva el commit automático para que no lo realice cada
+    # vez que se realiza un INSERT, UPDATE, etc
+    connection.autocommit = False
+
+    try:
+        cursor = connection.cursor();
+        ejecucion(connection, cursor)
+    except Exception as e:
+        print(e)
+
+    # Aunque se ejecute correctamente el try o el except, siempre va a ejecutar 
+    # el código que hay en el finally.
+    finally:
+        cursor.close()
+        connection.close()
+        print("Programa finalizado")
