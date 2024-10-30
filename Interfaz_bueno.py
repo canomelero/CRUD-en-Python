b7ea809509083a6538c2f77300c3a39c2520cd52
jorge <jcanomelero@gmail.es>
2024-10-30 13:29:42 +0100

    opcion 3 casi acabada, falta poner el botón para volver

--- Interfaz_bueno.py
+++ Interfaz_bueno.py
@@ -17,7 +17,12 @@ imagen_fondo = tk.PhotoImage(file="./PantallaInicio.png")
 pantallaInicial = tk.Frame(app, bg = "lightblue")   
 menuPrincipal = tk.Frame(app, bg = "lightcoral")
 segundoMenu = tk.Frame(app, bg = "lightgreen")
-pantallaOpc3 = tk.Frame(app, bg = "black")
+pantallaOpc1 = tk.Frame(app, bg = "lightgreen")
+pantallaOpc3 = tk.Frame(app, bg = "blue")
+frameTabla = tk.Frame(app)
+frameTablaStock = tk.Frame(app)
+frameTablaPedido = tk.Frame(app)
+frameTablaDetPed = tk.Frame(app)
 
 # Añadir la imagen de fondo de la pantalla principal
 label_fondo = tk.Label(pantallaInicial, image=imagen_fondo)
@@ -29,43 +34,199 @@ def mostrarPantalla(frame):
     # pack_forget() -> Método de la clase Frame para ocutlar un widget que ha sido posicionado en la interfaz
     pantallaInicial.pack_forget()
     menuPrincipal.pack_forget()
+    pantallaOpc1.pack_forget()
     pantallaOpc3.pack_forget()
+    frameTabla.pack_forget()
+    frameTablaStock.pack_forget()
+    frameTablaPedido.pack_forget()
+    frameTablaDetPed.pack_forget()
 
     # pack() -> Método de la clase Frame que muestra un widget en la pantalla
     frame.pack(fill = 'both', expand = True) 
 
 
-def cargarTablasBD():
-    mostrarPantalla(pantallaOpc3)
+def creacionBorradoTablas(archivo):
+    pedidos.crear_tablas(archivo.get()) 
+    mostrarPantalla(menuPrincipal)
+
+
+def accionPrincipal1():
+    mostrarPantalla(pantallaOpc1)
+
+    # Creación del Frame que contendrá la información
+    frameAux = tk.Frame(pantallaOpc1, bg = "lightgreen")
+    frameAux.pack(expand = True)
+
+    mensaje = tk.Label(frameAux, text = "Indique el nombre archivo .sql desde \nla ruta actual (ponga /nombre_archivo.sql)",
+                       bg = "lightgreen", font=("Arial", 16))
+    mensaje.pack()
+
+    respuesta = tk.Entry(frameAux, width = 30)
+    respuesta.pack(pady = 20)
+
+    pedidos.cursor.execute("SAVEPOINT s1;")
+
+    btnConfirmar = tk.Button(
+        frameAux, 
+        text = "Confirmar",
+        width = 7,
+        height = 3,
+        relief = "groove", 
+        borderwidth = 2,
+        bg = "#CCFF99",
+        font=("Arial", 12),
+        command = lambda : creacionBorradoTablas(respuesta)
+    )
+
+    # Ubicación de botón en el centro de la pantalla
+    btnConfirmar.pack(pady = 10)
+
+
+def cargarTablaStock():
+    mostrarPantalla(frameTablaStock)
     
-    #pedidos.mostrar_tablas("SELECT * FROM ")
-    pedidos.cursor.execute(f"SELECT * FROM pedido LIMIT 0")
+    pedidos.cursor.execute(f"SELECT * FROM stock LIMIT 0")
     columns = [description[0] for description in pedidos.cursor.description]
 
-    tree = ttk.Treeview(pantallaOpc3, columns=columns, show="headings")
+    tablaStock = pedidos.obtener_stock()
+    
+    # Crear un sub-frame específico para la tabla
+    frameTablaAux = tk.Frame(frameTablaStock)
+    frameTablaAux.pack(expand = True, fill = "both")
+
+    tree = ttk.Treeview(frameTablaAux, columns=columns, show="headings")
     tree.grid(row=0, column=0, sticky="nsew")
 
     for col in columns:
         tree.heading(col, text=col)
         tree.column(col, width=100, anchor="center")
 
-    #for row in rows:
-    #    tree.insert("", "end", values=row)
+    for row in tablaStock:
+        tree.insert("", "end", values=row)
 
-    pantallaOpc3.grid_rowconfigure(0, weight = 1)
-    pantallaOpc3.grid_columnconfigure(0, weight = 1)
+    frameTablaAux.grid_rowconfigure(0, weight = 1)
+    frameTablaAux.grid_columnconfigure(0, weight = 1)
 
+
+def cargarTablaPedio():
+    mostrarPantalla(frameTablaPedido)
+    
+    pedidos.cursor.execute(f"SELECT * FROM pedido LIMIT 0")
+    columns = [description[0] for description in pedidos.cursor.description]
+
+    tablaPedido = pedidos.obtener_pedido()
+
+    # Crear un sub-frame específico para la tabla
+    frameTablaAux = tk.Frame(frameTablaPedido)
+    frameTablaAux.pack(expand = True, fill = "both")
+
+    tree = ttk.Treeview(frameTablaAux, columns=columns, show="headings")
+    tree.grid(row=0, column=0, sticky="nsew")
+
+    for col in columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100, anchor="center")
+
+    for row in tablaPedido:
+        tree.insert("", "end", values=row)
+
+    frameTablaAux.grid_rowconfigure(0, weight = 1)
+    frameTablaAux.grid_columnconfigure(0, weight = 1)
+
+
+def cargarTablaDetallePed():
+    mostrarPantalla(frameTablaDetPed)
+    
+    pedidos.cursor.execute(f"SELECT * FROM detalle_pedido LIMIT 0")
+    columns = [description[0] for description in pedidos.cursor.description]
+
+    tablaDetallePed = pedidos.obtener_detalle_pedido()
+
+    # Crear un sub-frame específico para la tabla
+    frameTablaAux = tk.Frame(frameTablaDetPed)
+    frameTablaAux.pack(expand = True, fill = "both")
+
+    tree = ttk.Treeview(frameTablaAux, columns=columns, show="headings")
+    tree.grid(row=0, column=0, sticky="nsew")
+
+    for col in columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100, anchor="center")
+
+    for row in tablaDetallePed:
+        tree.insert("", "end", values=row)
+
+    frameTablaAux.grid_rowconfigure(0, weight = 1)
+    frameTablaAux.grid_columnconfigure(0, weight = 1)
+
+
+def cargarTabla(opcion):
+    if opcion.get() == 1:
+        cargarTablaPedio()
+    elif opcion.get() == 2:
+        cargarTablaStock()
+    else :
+        cargarTablaDetallePed()
+    
+    btnConfirmar = tk.Button(
+        frameTabla, 
+        text = "Volver",
+        width = 7,
+        height = 3,
+        relief = "groove", 
+        borderwidth = 2,
+        bg = "#CCFF99",
+        font=("Arial", 12),
+        command = lambda : mostrarPantalla(pantallaOpc3)    
+    )
+
+    btnConfirmar.pack(pady = 20)
+    
+
+def accionPrincipal3():
+    mostrarPantalla(pantallaOpc3)
+
+    # Crear un sub-Frame para centrar los Radiobuttons usando grid
+    frameOpcion = tk.Frame(pantallaOpc3, bg = "blue")
+    frameOpcion.pack(expand = True)  # Pack dentro del frame_principal
+
+    opcion1 = tk.Radiobutton(frameOpcion, text = "Tabla Pedido", 
+                             variable = opcion, value = 1, bg = "blue", highlightthickness = 0, font=("Arial", 16))
+    opcion2 = tk.Radiobutton(frameOpcion, text = "Tabla Stock", variable = opcion, value = 2,
+                             bg = "blue", highlightthickness = 0, font=("Arial", 16))
+    opcion3 = tk.Radiobutton(frameOpcion, text = "Tabla Detalle - Pedido", variable = opcion, 
+                             value = 3, bg = "blue", highlightthickness = 0, font=("Arial", 16))
+
+    opcion1.pack(pady = 10)
+    opcion2.pack(pady = 10)
+    opcion3.pack(pady = 10)
+
+    btnConfirmar = tk.Button(
+        frameOpcion, 
+        text = "Confirmar",
+        width = 7,
+        height = 3,
+        relief = "groove", 
+        borderwidth = 2,
+        bg = "#CCFF99",
+        font=("Arial", 12),
+        command = lambda : cargarTabla(opcion)    
+    )
+
+    btnConfirmar.pack(pady = 20)
 
 def realizarAccion():
     seleccion = opcion.get()
 
     if seleccion == 1:
-        print("Opcion 1")
+        accionPrincipal1()
     elif seleccion == 2:
         pedidos.aniadir_pedido()
         cargarSegundoMenu(segundoMenu)
     elif seleccion == 3:
-        cargarTablasBD()
+        #cargarTablaStock()
+        #cargarTablaPedio()
+        accionPrincipal3()
     elif seleccion == 4:
         print("Cerrando conexión...")
         pedidos.cerrar_conex()
@@ -191,6 +352,7 @@ if __name__ == "__main__":
     
     # mainloop() -> método encargado de ir actualizando la pantalla 
     app.mainloop()     
+
 
 
 
